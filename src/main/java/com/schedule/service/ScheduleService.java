31799571bbf1fb5e7ef6770572e4482493baa89e
junbeom95-lee <junbeom95@gmail.com>
2025-11-05 00:06:42 +0900

    feat(service): 일정 관리 서비스 생성
    1. @Service 추가
    2. TODO 일정 생성(CREATE) 주석 추가

---
+++ src/main/java/com/schedule/service/ScheduleService.java
@@ -0,0 +1,18 @@
+package com.schedule.service;
+
+import com.schedule.repository.ScheduleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor //final 로 선언된 속성을 자동으로 생성자로 생성해줘요
+public class ScheduleService {
+
+    //속성
+    private final ScheduleRepository scheduleRepository;
+
+    //생
+
+    //기
+    //TODO 일정 생성
+}
