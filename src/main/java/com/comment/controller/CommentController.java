369af2fab918ac90e1dfbbe18effc58d3a05b2e8
junbeom95-lee <junbeom95@gmail.com>
2025-11-05 20:50:22 +0900

    chore(lv5): 댓글 생성 TODO 계획 설계 및 클래스 생성
    1. 클래스 Controller, Service, Repository 생성
    2. 댓글 생성 API 기능 설계
    3. Controller
     a. method - POST
     b. url - "/scheudles/{id}/comments"
     c. request - 댓글 내용, 작성자명, 비밀번호
     d. response - 댓글id, 댓글 내용, 작성자명, 일정 고유식별자 작성일, 수정일
    4. Service
     a. request -> entity 변환
     b. entity -> repo를 통해 댓글 저장
     c. 저장한 entity를 응당 response로 변환 및 반환

---
+++ src/main/java/com/comment/controller/CommentController.java
@@ -0,0 +1,10 @@
+package com.comment.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommentController {
+
+    //TODO 댓글 생성
+    //TODO request - 댓글 내용, 작성자명, 비밀번호 일정 고유 ID
+}
---
+++ src/main/java/com/comment/service/CommentService.java
@@ -0,0 +1,12 @@
+package com.comment.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+
+    //TODO 댓글 생성
+    //TODO 1. request -> entity 변환
+    //TODO 2. entity를 repo에 댓글 저장
+    //TODO 3. entity를 응답 response로 변환 및 반환
+}
