6c89b946a9b769661d6a62f6d5f30e91dce5eeab
junbeom95-lee <junbeom95@gmail.com>
2025-11-05 10:30:35 +0900

    chore(repo): repo 주석 추가
    1. 데이터베이스 작업 담당

--- src/main/java/com/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/schedule/repository/ScheduleRepository.java
@@ -3,5 +3,8 @@ package com.schedule.repository;
 import com.schedule.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * 데이터베이스 작업 담당
+ */
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 }
