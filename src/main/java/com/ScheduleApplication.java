3a53033c0d2dd234b4795c9eb147eb9765969a10
junbeom95-lee <junbeom95@gmail.com>
2025-11-05 23:55:20 +0900

    refactor(lv5): Application 이동
    1. 기존 com.schedule 에서 com으로 이동
    2. 스캔 범위를 확장해서 Comment 패키지의 컴포넌트와 엔티티를 스캔

--- src/main/java/com/ScheduleApplication.java
+++ src/main/java/com/ScheduleApplication.java
@@ -1,13 +1,11 @@
-package com.schedule;
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@EntityScan(basePackages = {"com.schedule.entity", "com.comment.entity"})
 public class ScheduleApplication {
 
     public static void main(String[] args) {
